bde7553aaaa087fe84c74c0ebf81a5e59b4af7af
gfaruk02 <gfaruk02@gmail.com>
2026-09-16 23:46:22 +0600

    create error and loading component and set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import './App.css'
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import Error from "./components/Error";
+import Loading from "./components/Loading";
 
 function Router() {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className="font-bold text-2xl">Hello World</div>,
+    element: <Layout/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        index:true,
+        Component: Home,
+        hydrateFallbackElement: <Loading/>
+      }
+      
+    ]
   },
 ]);
 
---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+const Error = () => {
+  return (
+    <div>Page not Found.</div>
+  );
+};
+
+export default Error;
